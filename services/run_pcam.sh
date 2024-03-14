f2f985c99c6d2891215f695b0f4db2b197679afc
mattsmiths <masmith56@wisc.edu>
2024-03-14 14:56:47 -0500

    Update run_pcam.sh
    Changing directory paths, adding condition statements for config file

--- services/run_pcam.sh
+++ services/run_pcam.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 source $HOME/.bashrc
-source $HOME/.virtualenvs/pollinatorcam/bin/activate
+source $HOME/.virtualenvs/autopolls/bin/activate
 
-cd $HOME/r/braingram/pollinatorcam
+cd $HOME/AP/pollinatorcam
 
 # exec here to use same PID to allow systemd watchdog
 #exec python3 -m pollinatorcam -l $1 -rdD
 # TODO flag for usb vs IP camera
-exec python3 -m pollinatorcam -cDrt -l $1 -v
+MODELSTATUS=`cat /home/pi/Desktop/configs | jq '.model_inference'`
+
+if [ $MODELSTATUS == 0 ]
+then
+	exec python3 -m pollinatorcam -cDfrt -l $1 -v
+else
+	exec python3 -m pollinatorcam -cDrt -l $1 -v
+fi
